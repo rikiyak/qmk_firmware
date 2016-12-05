34fa8910740625f1e7bb06813e81d1c2bbbda3b6
Rikiya <rikiya.kubo@toshiba.co.jp>
2016-12-05 10:34:06 +0900

    fixed, support a new macro "TO"

--- keyboards/ergodox/keymaps/rikiyak/keymap.c
+++ keyboards/ergodox/keymaps/rikiyak/keymap.c
@@ -33,8 +33,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,         KC_2,             KC_3,       KC_4,       KC_5,       TO(MDIA, ON_RELEASE),
-        KC_TAB,         KC_Q,         KC_W,             KC_E,       KC_R,       KC_T,       TO(SYMB, ON_RELEASE),
+        KC_ESC,         KC_1,         KC_2,             KC_3,       KC_4,       KC_5,       TO(MDIA),
+        KC_TAB,         KC_Q,         KC_W,             KC_E,       KC_R,       KC_T,       TO(SYMB),
         KC_LCTRL,       KC_A,         KC_S,             KC_D,       KC_F,       KC_G,
         KC_LSFT,        KC_Z,         KC_X,             KC_C,       KC_V,       KC_B,       KC_LGUI,
         KC_GRV,         KC_QUOT,      LALT(KC_LSFT),    KC_LEFT,    KC_RGHT,
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                             KC_END,
                                                                     KC_SPC,     KC_BSPC,    KC_HOME,
         // right hand
-        TO(MDIA, ON_RELEASE),   KC_6,           KC_7,       KC_8,       KC_9,       KC_0,               KC_MINS,
-        TO(SYMB, ON_RELEASE),   KC_Y,           KC_U,       KC_I,       KC_O,       KC_P,               KC_BSLS,
+        TO(MDIA),               KC_6,           KC_7,       KC_8,       KC_9,       KC_0,               KC_MINS,
+        TO(SYMB),               KC_Y,           KC_U,       KC_I,       KC_O,       KC_P,               KC_BSLS,
                                 KC_H,           KC_J,       KC_K,       KC_L,       KC_SCLN,            GUI_T(KC_QUOT),
         KC_RGUI,                KC_N,           KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,            KC_RSFT,
                                 KC_UP,          KC_DOWN,    KC_LBRC,    KC_RBRC,    KC_EQL,
@@ -75,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = KEYMAP(
        // left hand
-       M(0),   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  TO(MDIA, ON_RELEASE),
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,TO(BASE, ON_RELEASE),
+       M(0),   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  TO(MDIA),
+       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,TO(BASE),
        KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
        KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
@@ -84,8 +84,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                KC_TRNS,
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
-       TO(MDIA, ON_RELEASE),KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       TO(BASE, ON_RELEASE),KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
+       TO(MDIA),KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
+       TO(BASE),KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
                 KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
        KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
                          KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
@@ -116,8 +116,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // MEDIA AND MOUSE
 [MDIA] = KEYMAP(
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(BASE, ON_RELEASE),
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, TO(SYMB, ON_RELEASE),
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(BASE),
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, TO(SYMB),
        KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
@@ -125,8 +125,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     KC_TRNS,
                                   KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
-       TO(BASE, ON_RELEASE), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       TO(SYMB, ON_RELEASE), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       TO(BASE), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       TO(SYMB), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
                           KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
