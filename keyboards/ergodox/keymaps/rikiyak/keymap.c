fcadb5b6ebfe8b0cc4966d48d2f68b395cfba73d
Rikiya <rikiya.kubo@toshiba.co.jp>
2016-12-05 09:59:38 +0900

    modify the TAB space

--- keyboards/ergodox/keymaps/rikiyak/keymap.c
+++ keyboards/ergodox/keymaps/rikiyak/keymap.c
@@ -33,23 +33,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   TO(MDIA, ON_RELEASE),
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TO(SYMB, ON_RELEASE),
-        KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI,
-        KC_GRV,         KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
-                                               KC_ESC,        ALT_T(KC_APP),
-                                                              KC_END,
-                                               KC_SPC,KC_BSPC,KC_HOME,
+        KC_ESC,         KC_1,         KC_2,             KC_3,       KC_4,       KC_5,       TO(MDIA, ON_RELEASE),
+        KC_TAB,         KC_Q,         KC_W,             KC_E,       KC_R,       KC_T,       TO(SYMB, ON_RELEASE),
+        KC_LCTRL,       KC_A,         KC_S,             KC_D,       KC_F,       KC_G,
+        KC_LSFT,        KC_Z,         KC_X,             KC_C,       KC_V,       KC_B,       KC_LGUI,
+        KC_GRV,         KC_QUOT,      LALT(KC_LSFT),    KC_LEFT,    KC_RGHT,
+                                                                                KC_ESC,     ALT_T(KC_APP),
+                                                                                            KC_END,
+                                                                    KC_SPC,     KC_BSPC,    KC_HOME,
         // right hand
-             TO(MDIA, ON_RELEASE),    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-             TO(SYMB, ON_RELEASE),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
-             KC_RGUI,     KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_EQL,
-             KC_LALT,        CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,KC_TAB, KC_ENT
+        TO(MDIA, ON_RELEASE),   KC_6,           KC_7,       KC_8,       KC_9,       KC_0,               KC_MINS,
+        TO(SYMB, ON_RELEASE),   KC_Y,           KC_U,       KC_I,       KC_O,       KC_P,               KC_BSLS,
+                                KC_H,           KC_J,       KC_K,       KC_L,       LT(MDIA, KC_SCLN),  GUI_T(KC_QUOT),
+        KC_RGUI,                KC_N,           KC_M,       KC_COMM,    KC_DOT,     CTL_T(KC_SLSH),     KC_RSFT,
+                                KC_UP,          KC_DOWN,    KC_LBRC,    KC_RBRC,    KC_EQL,
+        KC_LALT,                CTL_T(KC_ESC),
+        KC_PGUP,
+        KC_PGDN,                KC_TAB,     KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
